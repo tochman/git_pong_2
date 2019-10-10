b3a2c89827bb766c76934cf2b7ba9762e8bf2f15
Thomas <thomas@craftacademy.se>
2019-10-10 10:24:28 +0200

    cleans up my typos :zap:

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-  let displayElement = document.getElementById('display')
-  displayElement.innerHTML = "Hello World 123"
-  displayElement.style.color = 'red'
-})
+  let displayElement = document.getElementById('display');
+  displayElement.innerHTML = "Hello World 123";
+  displayElement.style.color = 'red';
+});
